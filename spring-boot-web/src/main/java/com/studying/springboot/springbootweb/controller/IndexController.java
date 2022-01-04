33426a95af504d34699da3297d40304ec83ef803
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-01-03 20:25:58 -0700

    User model

--- spring-boot-web/src/main/java/com/studying/springboot/springbootweb/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/studying/springboot/springbootweb/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.studying.springboot.springbootweb.controller;
 
+import com.studying.springboot.springbootweb.model.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,20 @@ public class IndexController {
   public String index(Model model) {
     model.addAttribute("title", "Hello Spring Boot!");
     return "index";
+  }
+
+  @GetMapping("/profile")
+  public String profile(Model model) {
+    User user = new User();
+    user.setName("Andy");
+    user.setLastName("Alonso");
+    model.addAttribute("user", user);
+    StringBuilder data = new StringBuilder();
+    data.append(user.getName());
+    data.append(" ");
+    data.append(user.getLastName());
+    model.addAttribute("title", "Hello: ".concat(data.toString()));
+    return "profile";
   }
 
 }
---
+++ spring-boot-web/src/main/java/com/studying/springboot/springbootweb/model/User.java
@@ -0,0 +1,16 @@
+package com.studying.springboot.springbootweb.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+
+  private String name;
+  private String lastName;
+  
+  
+}
