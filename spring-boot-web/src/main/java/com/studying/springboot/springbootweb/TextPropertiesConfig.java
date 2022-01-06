03abde407cf777e1a65aa021ece98baeead77274
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-01-05 17:33:04 -0700

    Properties files

---
+++ spring-boot-web/src/main/java/com/studying/springboot/springbootweb/TextPropertiesConfig.java
@@ -0,0 +1,13 @@
+package com.studying.springboot.springbootweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+  @PropertySource("classpath:text.properties")
+})
+public class TextPropertiesConfig {
+  
+}
